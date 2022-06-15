abbc81c67669042b923480afbe609776448acc1c
XTsat <44708609+XTsat@users.noreply.github.com>
2022-06-16 02:11:21 +0800

    Favicon - base64
    Favicon 正常部署时要单独指定 base 链接，现在切换为 base64 实现模式

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,33 +9,17 @@ export default defineUserConfig({
   base: '/rapid-upload-userscript-doc/',
 
   head: [
-    // [
-    //   'link',
-    //   {
-    //     rel: 'icon',
-    //     type: 'image/png',
-    //     sizes: '16x16',
-    //     href: `/logo/favicon.ico`,
-    //   },
-    // ],
-    // [
-    //   'link',
-    //   {
-    //     rel: 'icon',
-    //     type: 'image/png',
-    //     sizes: '32x32',
-    //     href: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABBUlEQVR4AZTTJRBUURTH4TtDwXuPdPrgbhHXiksf3CPucRNScHd3d3d3uO9bKeu7b79+fun8Q17CNHyMMUqaiPE4fEyYVjjGNKnNwQ4lpgV8lManEfwfosLHEGPU1N3ZnAv4qlT+NiQ56uPWSjKBrztUSnIaB66sY1vgxgxoMXB5NbsCB9rxcB5fN2M5/16nCFxeS6YTezpzsB1Pu/C2O7/78/99eYBYHXh+gqdHObGIK4GHgevjVIt1AgAnhvE4cGe8euoHbizgYuD2RGgx8O0RpwIPRmsmJDGqcrANd3pLo/qVr03hUlcpfSwf0/vD3JwkPdPK5/zhkOz+/f1FIDv/RcnOAEjywH/DhgADAAAAAElFTkSuQmCC`,
-    //   },
-    // ],
+    
     [
       'link',
       {
         rel: 'icon',
         type: 'image/png',
-        sizes: '32x32',
-        href: '/rapid-upload-userscript-doc/logo/logo.png',
+        sizes: '16x16',
+        href: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABBUlEQVR4AZTTJRBUURTH4TtDwXuPdPrgbhHXiksf3CPucRNScHd3d3d3uO9bKeu7b79+fun8Q17CNHyMMUqaiPE4fEyYVjjGNKnNwQ4lpgV8lManEfwfosLHEGPU1N3ZnAv4qlT+NiQ56uPWSjKBrztUSnIaB66sY1vgxgxoMXB5NbsCB9rxcB5fN2M5/16nCFxeS6YTezpzsB1Pu/C2O7/78/99eYBYHXh+gqdHObGIK4GHgevjVIt1AgAnhvE4cGe8euoHbizgYuD2RGgx8O0RpwIPRmsmJDGqcrANd3pLo/qVr03hUlcpfSwf0/vD3JwkPdPK5/zhkOz+/f1FIDv/RcnOAEjywH/DhgADAAAAAElFTkSuQmCC`,
       },
     ],
+    
   ],
 
   // site-level locales config
